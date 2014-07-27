0cbcb2c921eab86568358227c59f10975405cd5b
nezroy <nezroy@gmail.com>
2014-07-27 01:10:18 -0600

    fixing bogus filename

--- minify.bat
+++ minify.bat
@@ -1,4 +1,4 @@
-copy /b src\core.js+src\SDD.js+src\SDD.Source+src\SDD.Source.json.js+src\SDD.Table.js+src\IGB.js dist\EVEoj.js
+copy /b src\core.js+src\SDD.js+src\SDD.Source.js+src\SDD.Source.json.js+src\SDD.Table.js+src\IGB.js dist\EVEoj.js
 copy /b src\map.js+src\map.System.js dist\EVEoj.map.js
 
 java -jar D:\Projects\yuicompressor-2.4.8.jar -v --charset utf-8 -o dist\EVEoj.min.js dist\EVEoj.js
